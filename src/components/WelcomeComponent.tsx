66a8ceb8da6148d47a1bf3887f991fbf95cdc5c7
Camilo Gil Franco <camilogilfranco@gmail.com>
2023-08-26 16:45:52 -0500

    Se agrega parrafo explicativo

--- src/components/WelcomeComponent.tsx
+++ src/components/WelcomeComponent.tsx
@@ -8,6 +8,11 @@ const WelcomeComponent: React.FC = () => {
     <div className="flex flex-col items-center mt-8">
       <Image src={welcome} alt="" width={330} />
       <h2 className="text-5xl font-bold mt-8">Welcome</h2>
+      <p className="text-center w-96">
+        This is a dictionary of characters from the animated series Rick and
+        Morty, you must write a word in the search engine and all the characters
+        whose names match your search will be displayed.
+      </p>
     </div>
   );
 };
